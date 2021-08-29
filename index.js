30a6a0e5e8ff6b8a4c0bd75ff62727c53e7b65b6
mert <m.nesvat@kentech-sp.com>
2021-08-29 19:49:37 +0100

    quote is added

--- index.js
+++ index.js
@@ -19,6 +19,47 @@ app.get('/', function(req, res) {
 
 });
 
+app.get('/quote', function(req, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('X-Frame-Options', 'DENY');
+    res.header('X-XSS-Protection', '1; mode=block');
+    res.header('X-Content-Type-Options', 'nosniff');
+    res.header('Strict-Transport-Security', 'max-age=63072000');
+    res.setHeader('Content-Type', 'application/json');
+    app.disable('x-powered-by');
+
+    axios({
+        method: 'GET',
+        url: 'https://randomword.com/3-word-quotes',
+        headers: {
+            'User-Agent': rua
+        }
+    }).then(function(response) {
+        $ = cheerio.load(response.data);
+
+        var post = $('.section #shared_section');
+        var word = post.find('#random_word').eq(0).text().replace('\r\n\t\t\t\t\t', '').replace('\r\n\t\t\t\t', '').replace('\n\t\t\t\t\t', '').replace('\n\t\t\t\t', '');
+
+        var wordOfDay = {
+            quote: decodeURI(word.charAt(0).toUpperCase() + word.slice(1))
+        }
+
+        console.log("User-Agent:", rua);
+        res.send(JSON.stringify(wordOfDay, null, 2));
+        console.log(wordOfDay);
+    }).catch(function(error) {
+        if (!error.response) {
+            console.log('API URL is Missing');
+            res.json('API URL is Missing');
+        } else {
+            console.log('Something Went Wrong - Enter the Correct API URL');
+            res.json('Something Went Wrong - Enter the Correct API URL');
+        }
+    });
+});
+
 app.get('/idiom', function(req, res) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
